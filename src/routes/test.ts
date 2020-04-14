b2b1826cd72bca472573eca4bc1679ba9c51cb05
lgh06 <hnnk@qq.com>
2020-04-14 15:55:47 +0800

    revert route

---
+++ src/routes/test.ts
@@ -0,0 +1,54 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable import/no-named-default */
+import { Next, default as Koa } from 'koa'
+import {
+  util,
+  decorators,
+} from '@edgefront/api'
+import { TestService } from '../services'
+
+
+const {
+  Controller, Get, Autowired, Post,
+} = decorators
+@Controller()
+export default class TestController {
+  @Autowired()
+  public testService: TestService
+
+  @Get('test1')
+  async someGetMethod(ctx: Koa.Context, next: Next): Promise<void> {
+    const result = await this.testService.dbService()
+    ctx.body = `Hello World 1!${JSON.stringify(result)} ${this.testService.testMethod(666)}`
+    await next()
+  }
+
+  @Get('')
+  someGetMethod2(ctx: Koa.Context): void{
+    ctx.body = `Hello World 2!${JSON.stringify(ctx.header)}`
+  }
+
+  @Get('db')
+  async someGetMethod3(ctx: Koa.Context): Promise<void> {
+    ctx.body = await this.testService.dbService()
+  }
+
+  @Post()
+  login(ctx: Koa.Context, next: Next): Promise<void> {
+    const { username, password } = ctx.request.body
+    if (username === '1' && password === '1') {
+      const token = ctx.jwt.sign(
+        { username },
+        util.getConfig('SECRET'),
+        {
+          expiresIn: '24h', // expires in 24 hours
+        },
+      )
+      ctx.body = { token }
+      return next()
+    }
+    ctx.response.status = 401
+    ctx.body = 'login failed'
+    return next()
+  }
+}
